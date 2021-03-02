b65eb418e2dad0362661578c98d0363bf4c5f4fd
Lucas Fridez <lucas.fridez@he-arc.ch>
2021-03-02 16:36:19 +0100

    Style: Improve 'Zest your life' title on Home Page

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container id="share-page" fill-height fill-width fluid class="background-accent-gradient d-flex align-center justify-center flex-col flex-col">
-    <h2>Zest your life</h2>
+    <h2><span class="fw-700">Z</span>est your life</h2>
     <div class="d-flex nowrap align-center justify-center">
       <v-text-field
         prepend-icon="mdi-magnify"
