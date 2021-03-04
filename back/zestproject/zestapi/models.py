dd76f961bf00b5d2f24e76dd2b0ee1e1251bc590
Steve Mendes Reis <steve.mendesreis@he-arc.ch>
2021-03-04 20:43:33 +0100

    add: wip impl. database

--- back/zestproject/zestapi/models.py
+++ back/zestproject/zestapi/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+class Ressource(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    picture = models.CharField(max_length=255)
+    date_start = models.DateField()
+    date_end = models.DateField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    ressource_id = models.CharField(255)
+    participants = models.ManyToManyField(Person, through='Participate')
+    user = models.ForeignKey(User) 
+
+class Participate(models.Model):
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
