67763ef7ef658ce768fe969c9db82ebebe2ab059
Lucas Fridez <lucas.fridez@he-arc.ch>
2021-03-12 11:03:06 +0100

    Update: texUser Card in Profile view

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -3,19 +3,29 @@
     <v-layout justify-center wrap>
       <v-flex xs12 md8>
         <TitledCard
-          color="general"
-          title="Edit Profile"
-          text="Complete your profile"
+          title="My resources"
         >
           <v-form>
             <v-container py-0>
               <v-layout wrap>
-                <v-flex xs12 md4>
-                  <v-text-field label="Company (disabled)" disabled />
-                </v-flex>
-                <v-flex xs12 md4>
-                  <v-text-field class="purple-input" label="User Name" />
-                </v-flex>
+
+                <p class="mt-4">Below appears all of your resources ! Have a look at them and maybe you can share another one !</p>
+                <v-row class="mb-2">
+                  <v-col
+                    sm="12"
+                    md="6"
+                    lg="6"
+                    xl="4"
+                    v-for="i in Array.from(Array(5).keys())"
+                    :key="i"
+                  >
+                    <Resource
+                      :title="'Caquelon ' + (i + 1)"
+                      authorName="Lucas Fridez"
+                      :nbPeople="10 - i"
+                    />
+                  </v-col>
+                </v-row>
               </v-layout>
             </v-container>
           </v-form>
@@ -23,18 +33,19 @@
       </v-flex>
       <v-flex xs12 md4>
         <TitledCard class="v-card-profile">
-          <Avatar slot="offset" class="mx-auto d-block" fullname="Lucas Fridez" size="130">
+          <Avatar
+            slot="offset"
+            class="mx-auto d-block"
+            fullname="Lucas Fridez"
+            size="130"
+          >
           </Avatar>
           <v-card-text class="text-xs-center">
-            <h3 class="card-title font-weight-light text-center">Fridez Lucas</h3>
-            <p class="card-description font-weight-light">
-              You wrote that yourself? wow congrats dude, really, that's very
-              cool. i just told everyone in my family about it, everybody thinks
-              that's very impressive and asked me to congratulate you. they want
-              to speak to you in person, if possible, to give you their regards.
-              they also said they will tell our distant relatives that can make
-              your name stand out more and see which city can congratulate you
-              the hardest for this magnificent feat.
+            <h3 class="card-title font-weight-light text-center">
+              Fridez Lucas
+            </h3>
+            <p class="card-description font-weight-light text-center">
+              You need to change personnal information ? The form below can help you !
             </p>
           </v-card-text>
         </TitledCard>
@@ -46,11 +57,12 @@
 <script lang="js">
 import Vue from "vue";
 import TitledCard from "../components/TitledCard"
+import Resource from "../components/Resource"
 import Avatar from "../components/Avatar"
 
 export default Vue.extend({
     name: "Home",
-    components: {TitledCard, Avatar},
+    components: {TitledCard, Avatar, Resource},
     methods: {
         //
     },
