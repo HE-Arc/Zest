caf4fc72ba2a6b3c4ea3e0f5f97c6fec486f0f62
Lucas Fridez <lucas.fridez@he-arc.ch>
2021-03-15 10:20:22 +0100

    Add: default methods for Vuex store

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,21 +1,42 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import Api from "./../logic/api/ApiRequester";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
+// https://pusher.com/tutorials/authentication-vue-vuex#set-up-vuex-actions
 export default new Vuex.Store({
   state: {
-    loggedIn: false,
     fullname: "Adam Smith",
+    loggedIn: false,
+    status: "",
     token: "",
   },
   mutations: {
+    authenticationSuccess(state, token, user) {
+      // TODO
+    },
+    authenticationError(state) {
+      // TODO
+    },
+    logout(state) {
+      state.token = "";
+    },
   },
   actions: {
+    login({ commit }, user) {
+      // TODO
+    },
+    register({ commit }, user) {
+      // TODO
+    },
+    logout({ commit }) {
+      // TODO
+    },
   },
-  modules: {
-  },
+  modules: {},
   getters: {
-
-  }
-})
+    isLoggedIn: (state) => !!state.token,
+    authStatus: (state) => state.status,
+  },
+});
