6d27a9e6d706d33b2df44e03d749834c25c90ec8
Lucas Fridez <lucas.fridez@he-arc.ch>
2021-03-15 08:36:47 +0100

    Add: Header component

---
+++ front/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <router-link to="/">
+      <div class="d-flex align-center">
+        <img :src="require('./../assets/logo.svg')" height="42" class="my-3" />
+        <h1 class="ml-3">Zest</h1>
+      </div>
+    </router-link>
+
+    <v-spacer></v-spacer>
+    <router-link to="/" class="white--text">Home</router-link> |
+    <router-link to="/Login" class="white--text">Login</router-link>
+    <router-link to="/SignUp" class="white--text">SignUp</router-link>
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<!-- SCRIPT -->
+<script lang="js">
+import Vue from "vue";
+export default Vue.extend({
+    name: "Header",
+    methods: {
+        //
+    }
+});
+</script>
