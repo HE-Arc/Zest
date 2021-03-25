b65821c3bda8d877792cd7e2ecdfe599e4e73252
Steve Mendes Reis <steve.mendesreis@he-arc.ch>
2021-03-25 20:57:18 +0100

    Edit: custom get token missing after merge

--- back/zestproject/zestapi/urls.py
+++ back/zestproject/zestapi/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
                             })),
 
     # AUTH JWT
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
